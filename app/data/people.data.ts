c47ce218978c95f72248501884d9751692a914d2
Michael Dziedzic <michaeldzie@gmail.com>
2016-02-15 18:11:02 -0800

    data: entered all people data

--- app/data/people.data.ts
+++ app/data/people.data.ts
@@ -3,56 +3,191 @@ import { Person } from '../interfaces/person';
 export var PEOPLE: Person[] = [
   {
     "id": 0,
-    "firstName": "Steven",
-    "lastName": "Tsao",
-    "location": "Portland, OR",
-    "timeDiff": 0,
-    "pairs": [1, 2],
-    "image": "steven"
+    "firstName": "Aimee",
+    "lastName": "Rosato",
+    "location": "New York, NY",
+    "timeDiff": -3,
+    "pairs": [11, 14, 13, 12, 10, 8, 6, 2, 16, 5, 17],
+    "image": "aimee"
   },
   {
     "id": 1,
-    "firstName": "Tim",
-    "lastName": "Dang",
-    "location": "Irvine, CA",
-    "timeDiff": 0,
-    "pairs": [0, 2],
-    "image": "tim"
+    "firstName": "Alex",
+    "lastName": "Ko",
+    "location": "Denver, CO",
+    "timeDiff": -2,
+    "pairs": [13, 6, 8, 19, 16, 3, 12, 5, 14, 11, 9, 2],
+    "image": "alex"
   },
   {
     "id": 2,
-    "firstName": "Michael",
-    "lastName": "Dziedzic",
-    "location": "Seattle, WA",
-    "timeDiff": 0,
-    "pairs": [0, 1],
-    "image": "michael"
+    "firstName": "Andrew",
+    "lastName": "Vedady",
+    "location": "Chicago, IL",
+    "timeDiff": -2,
+    "pairs": [6, 18, 7, 17, 11, 5, 16, 4, 20, 0, 14, 13, 1],
+    "image": "andrew"
   },
   {
     "id": 3,
-    "firstName": "Tyler",
-    "lastName": "Cookson",
-    "location": "San Francisco, CA",
-    "timeDiff": 0,
-    "pairs": [4, 5],
-    "image": "tyler"
+    "firstName": "Kris",
+    "lastName": "Burke",
+    "location": "Lexington, KY",
+    "timeDiff": -3,
+    "pairs": [20, 17, 12, 10, 14, 1, 19, 4, 13, 6, 8, 5],
+    "image": "kris"
   },
   {
     "id": 4,
-    "firstName": "John",
-    "lastName": "Soo",
-    "location": "Phoenix, AZ",
-    "timeDiff": 0,
-    "pairs": [3, 5],
-    "image": "john"
+    "firstName": "Brian",
+    "lastName": "Moore",
+    "location": "Maine",
+    "timeDiff": -3,
+    "pairs": [12, 9, 13, 8, 6, 7, 2, 3, 18, 20, 17, 13],
+    "image": "brian"
   },
   {
     "id": 5,
+    "firstName": "Christopher",
+    "lastName": "Patton",
+    "location": "Temple, GA",
+    "timeDiff": -3,
+    "pairs": [17, 7, 18, 15, 16, 2, 20, 1, 12, 11, 0, 3],
+    "image": "chris"
+  },
+  {
+    "id": 6,
+    "firstName": "Colton",
+    "lastName": "Savage",
+    "location": "Anytown, USA",
+    "timeDiff": 0,
+    "pairs": [2, 1, 10, 20, 9, 4, 12, 13, 0, 17, 3, 14, 7],
+    "image": "colton"
+  },
+  {
+    "id": 7,
     "firstName": "Dan",
     "lastName": "Molitor",
     "location": "Los Angeles, CA",
     "timeDiff": 0,
-    "pairs": [3, 4],
+    "pairs": [16, 5, 2, 18, 20, 13, 4, 18, 8, 15, 18, 6],
     "image": "dan"
-  }
+  },
+  {
+    "id": 8,
+    "firstName": "Elizabeth",
+    "lastName": "Sciortino",
+    "location": "New York, NY",
+    "timeDiff": -3,
+    "pairs": [19, 15, 1, 4, 11, 0, 20, 7, 10, 18, 3, 9],
+    "image": "elizabeth"
+  },
+  {
+    "id": 9,
+    "firstName": "Eric",
+    "lastName": "Broberg",
+    "location": "San Luis Obispo, CA",
+    "timeDiff": 0,
+    "pairs": [10, 11, 4, 19, 6, 20, 18, 14, 12, 15, 5, 1, 8],
+    "image": "eric"
+  },
+  {
+    "id": 10,
+    "firstName": "Fanny",
+    "lastName": "Vasquez",
+    "location": "Salem, OR",
+    "timeDiff": 0,
+    "pairs": [9, 12, 6, 3, 15, 0, 11, 16, 19, 8, 17, 19],
+    "image": "fanny"
+  },
+  {
+    "id": 11,
+    "firstName": "Gordon",
+    "lastName": "Buhler",
+    "location": "Indiana",
+    "timeDiff": -3,
+    "pairs": [0, 9, 20, 14, 2, 8, 10, 17, 18, 16, 1, 15],
+    "image": "gordon"
+  },
+  {
+    "id": 12,
+    "firstName": "Ian",
+    "lastName": "Akers",
+    "location": "Norwalk, CT",
+    "timeDiff": -3,
+    "pairs": [4, 10, 3, 16, 0, 18, 6, 1, 9, 5, 14, 20, 18],
+    "image": "ian"
+  },
+  {
+    "id": 13,
+    "firstName": "J.T.",
+    "lastName": "Yim",
+    "location": "New York, NY",
+    "timeDiff": -3,
+    "pairs": [1, 15, 0, 4, 7, 6, 14, 3, 2, 6, 4],
+    "image": "jt"
+  },
+  {
+    "id": 14,
+    "firstName": "John",
+    "lastName": "Soo",
+    "location": "Phoenix, AZ",
+    "timeDiff": -1,
+    "pairs": [18, 0, 17, 11, 3, 19, 15, 9, 13, 1, 12, 2, 16],
+    "image": "john"
+  },
+  {
+    "id": 15,
+    "firstName": "Michael",
+    "lastName": "Dziedzic",
+    "location": "Seattle, WA",
+    "timeDiff": 0,
+    "pairs": [19, 13, 8, 5, 10, 17, 14, 9, 7, 16, 20, 11],
+    "image": "michael"
+  },
+  {
+    "id": 16,
+    "firstName": "Mike",
+    "lastName": "Frazier",
+    "location": "Chicago, IL",
+    "timeDiff": -2,
+    "pairs": [7, 20, 19, 12, 5, 1, 2, 10, 17, 11, 0, 15, 14],
+    "image": "mike"
+  },
+  {
+    "id": 17,
+    "firstName": "Sanford",
+    "lastName": "Bradshaw",
+    "location": "Houston, TX",
+    "timeDiff": -2,
+    "pairs": [5, 7, 14, 2, 18, 15, 19, 11, 16, 6, 10, 4, 0],
+    "image": "sanford"
+  },
+  {
+    "id": 18,
+    "firstName": "Steven",
+    "lastName": "Tsao",
+    "location": "Portland, OR",
+    "timeDiff": 0,
+    "pairs": [14, 2, 5, 7, 17, 12, 9, 7, 11, 4, 8, 7, 12],
+    "image": "steven"
+  },
+  {
+    "id": 19,
+    "firstName": "Tim",
+    "lastName": "Dang",
+    "location": "Irvine, CA",
+    "timeDiff": 0,
+    "pairs": [15, 8, 16, 9, 1, 14, 17, 3, 10, 20, 10, 20],
+    "image": "tim"
+  },
+  {
+    "id": 20,
+    "firstName": "Tyler",
+    "lastName": "Cookson",
+    "location": "San Francisco, CA",
+    "timeDiff": 0,
+    "pairs": [3, 16, 11, 6, 7, 9, 5, 8, 2, 19, 4, 12, 15, 19],
+    "image": "tyler"
+  },
 ];
